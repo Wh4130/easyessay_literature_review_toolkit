c795b5c3d5cbcbe5bb1cc3619bd6c9c2ff848295
Huang Lin Chun <huang0jin@gmail.com>
2025-07-13 17:41:43 +0800

    modified sidebar config back

--- index.py
+++ index.py
@@ -55,30 +55,7 @@ if "pinecone_idx_name" not in st.session_state:
 
 # * - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
 # *** Sidebar Config
-with st.sidebar:
-            
-            # * Icon & Title
-            text_box, icon_box = st.columns((0.7, 0.3))
-            with icon_box:
-                st.markdown(f'''
-                                <img class="image" src="data:image/jpeg;base64,{DataManager.image_to_b64(f"./pics/icon.png")}" alt="III Icon" style="width:500px;">
-                            ''', unsafe_allow_html = True)
-            with text_box:
-                st.write(" ")
-                st.header("Easy Essay")
-                st.caption(f"**Literature Review Tool**")
-
-            # * Pages
-            if st.session_state["logged_in"]:
-                st.page_link("index.py", label = 'Introduction & Demos', icon = ":material/info:")
-                st.page_link("./pages/page_docs.py", label = 'Literature Management', icon = ":material/folder_open:")
-                st.page_link("./pages/page_chat.py", label = 'Chat with Literature', icon = ":material/mark_chat_unread:")
-                st.page_link("./pages/page_upload.py", label = 'Upload & Summarize Literature', icon = ":material/edit_square:")
-                st.page_link("./pages/page_account.py", label = 'Account', icon = ":material/account_circle:")
-
-
-
-            Others.fetch_IP()   
+UIManager.render_sidebar()
 
 
 # * - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
